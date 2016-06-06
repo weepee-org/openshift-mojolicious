49d5433ce17029474be9c20ea3f2da8108b9dbee
Joeri van Dooren <ure@moreorless.be>
2016-06-06 14:59:54 +0200

    fix

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
-if [ $RUNTYPE="production" ]; then
+if [ $MOJO_MODE="production" ]; then
   PERL_ANYEVENT_RESOLV_CONF=resolv.conf hypnotoad -f mojo.pl
 else
-  PERL_ANYEVENT_RESOLV_CONF=resolv.conf morbo -f mojo.pl
+  PERL_ANYEVENT_RESOLV_CONF=resolv.conf morbo -m development -f mojo.pl -w /app/mojo.pl w /app/modules -v
 fi
