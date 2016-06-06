887454d0f4a1b30ea655e3ff90c64923d0e37b97
Joeri van Dooren <ure@moreorless.be>
2016-06-06 14:52:23 +0200

    production -> hypnotoad

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-hypnotoad -f mojo.pl
+if [ $RUNTYPE="production" ]; then
+  PERL_ANYEVENT_RESOLV_CONF=resolv.conf hypnotoad -f mojo.pl
+else
+  PERL_ANYEVENT_RESOLV_CONF=resolv.conf morbo -f mojo.pl
+fi
